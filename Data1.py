1cd1fc365e9e89d9ad046fdd83ae43ca53342568
amit-beast <58898493+amit-beast@users.noreply.github.com>
2019-12-16 11:44:22 +0530

    Data_Preprocessing
    1. Importing csv file
    2. Filling in missing data
    3. Encoding categorical data
    4. Doing feature scaling

--- Data1.py
+++ Data1.py
@@ -25,3 +25,13 @@ onehotencoder.fit(x)
 x=onehotencoder.transform(x).toarray()
 labelencoder_y = LabelEncoder()
 y=labelencoder_y.fit_transform(y)
+
+#Training Data and Test Data
+from sklearn.model_selection import train_test_split 
+x_train, x_test, y_train, y_test=train_test_split(x,y,test_size=0.2,random_state=0)
+
+#Feature_Scalinng
+from sklearn.preprocessing import StandardScaler
+sc_x=StandardScaler()
+x_train=sc_x.fit_transform(x_train)
+x_test=sc_x.transform(x_test)
