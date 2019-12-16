ec041744b58b87f2ce0f5a504c44898355e09efb
amit-beast <58898493+amit-beast@users.noreply.github.com>
2019-12-16 10:38:37 +0530

    Add files via upload

--- Data1.py
+++ Data1.py
@@ -1,4 +1,4 @@
-#Data Preprocessing
+#Data Preprocessing#
 #Importing the libraries
 
 import numpy as np #for importing mathematical operations
@@ -11,7 +11,17 @@ x = dataset.iloc[:,:-1].values
 y = dataset.iloc[:,3].values
 
 #Populatig the missing values
-from sklearn.preprocessing import Imputer
+from sklearn.preprocessing import Imputer 
 imputer=Imputer(missing_values="NaN",strategy="mean",axis=0)
 imputer.fit(x[:,1:3])
 x[:,1:3]=imputer.transform(x[:,1:3])
+
+#Encoding categorical data
+from sklearn.preprocessing import LabelEncoder,OneHotEncoder
+labelencoder_x = LabelEncoder()
+x[:,0]=labelencoder_x.fit_transform(x[:,0])
+onehotencoder=OneHotEncoder(categorical_features=[0])
+onehotencoder.fit(x)
+x=onehotencoder.transform(x).toarray()
+labelencoder_y = LabelEncoder()
+y=labelencoder_y.fit_transform(y)
